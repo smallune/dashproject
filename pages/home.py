6b13c5d0ed609164d5b5e60d47a7e9198044f32d
smallune <connerhsmall02@gmail.com>
2025-09-06 08:20:02 -0400

    style init

--- pages/home.py
+++ pages/home.py
@@ -1,9 +1,9 @@
 import dash
-from dash import html
+from dash import html, dcc
 
 dash.register_page(__name__, path = "/")
 
 layout = html.Div([
-    html.H2("Caffeine Dashboard"),
-    html.P("Team 14")
+    html.H2("Caffeine Dashboard", className = "centered-header"),
+    html.P("Team 14", className = "centered-header")
 ])
